79f803b72bf31e31fd1c327c253f848c7b0eb614
Vivek Nair <vivek@nair.me>
2022-04-24 14:19:24 -0700

    add rooms 7-9

--- Assets/Scenes/Teleporter.cs
+++ Assets/Scenes/Teleporter.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour {
     int index = 1;
     int b = 0;
+    int u = 0;
+    int m = 0;
     public GameObject b1;
     public GameObject b2;
     public GameObject b3;
     public AudioSource bs;
     public AudioClip pop;
+    public GameObject c1;
+    public GameObject c2;
+    public GameObject c3;
+    public GameObject c4;
+    public GameObject m1;
+    public GameObject m2;
+    public GameObject m3;
+    public GameObject m4;
+    public GameObject m5;
+    public GameObject m6;
+    public GameObject m7;
+    public GameObject m8;
+    public GameObject m9;
+    public GameObject m10;
 
     void Start() {
       this.transform.position = new Vector3(-7.698f, -3.5f, -17.471f);  // room 1 - hello
@@ -22,7 +38,10 @@ public class Teleporter : MonoBehaviour {
         new Vector3(-7.698f, -3.5f, -3.044f),   // room 3 - captcha - velvet
         new Vector3(-10.701f, 0f, -4.24f),   // room 4 - find letters - church
         new Vector3(10.391f, 0f, -4.24f),   // room 5 - color vision - daisy
-        new Vector3(25.165f, 0f, -9.365f)   // room 6 - proximity - red
+        new Vector3(25.165f, 0f, -9.365f),   // room 6 - proximity - red
+        new Vector3(11.584f, 0f, -16.135f),   // room 7 - MOCA - recluse
+        new Vector3(-25.475f, 0f, -7.88f),   // room 8 - wingspan - cave
+        new Vector3(-15.587f, 0f, -17.425f)   // room 9 - fitness - motivation
       };
 
       if (Input.GetKeyDown("space")) {
@@ -43,9 +62,47 @@ public class Teleporter : MonoBehaviour {
 
       if (Input.GetKeyDown("r")) {
         b = 0;
+        u = 0;
+        m = 0;
         b1.SetActive(true);
         b2.SetActive(true);
         b3.SetActive(true);
+        c1.SetActive(true);
+        c2.SetActive(true);
+        c3.SetActive(true);
+        c4.SetActive(true);
+        m1.SetActive(true);
+        m2.SetActive(true);
+        m3.SetActive(true);
+        m4.SetActive(true);
+        m5.SetActive(true);
+        m6.SetActive(true);
+        m7.SetActive(true);
+        m8.SetActive(true);
+        m9.SetActive(true);
+        m10.SetActive(true);
+      }
+
+      if (Input.GetKeyDown("u")) {
+        u++;
+        if (u == 1) c1.SetActive(false);
+        if (u == 2) c2.SetActive(false);
+        if (u == 3) c3.SetActive(false);
+        if (u == 4) c4.SetActive(false);
+      }
+
+      if (Input.GetKeyDown("m")) {
+        m++;
+        if (m == 1) m1.SetActive(false);
+        if (m == 2) m2.SetActive(false);
+        if (m == 3) m3.SetActive(false);
+        if (m == 4) m4.SetActive(false);
+        if (m == 5) m5.SetActive(false);
+        if (m == 6) m6.SetActive(false);
+        if (m == 7) m7.SetActive(false);
+        if (m == 8) m8.SetActive(false);
+        if (m == 9) m9.SetActive(false);
+        if (m == 10) m10.SetActive(false);
       }
     }
 }
